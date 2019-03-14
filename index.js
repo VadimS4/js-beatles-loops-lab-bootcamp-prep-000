316da2c7e5fdf99870cca970ef627a8d8d327538
Vadim Stakhnyuk <vstakhnyuk@outlook.com>
2019-03-14 09:22:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ function johnLennonFacts(){
   var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a chior boy and boy scout", "He hated the sound of his own voice"];
   var newFacts = [];
   var i = 0;
-  while(i < facts.length)
+  while(i < facts.length){
+    
+  }
   
   
 }
